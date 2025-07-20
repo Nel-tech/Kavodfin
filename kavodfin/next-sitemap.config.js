e76a58b6f6e208efea4558e9eee590c2397f7a5c
Nel-tech <favournelson82@gmail.com>
2025-07-20 07:01:21 +0100

    Add additional paths to sitemap configuration and update sitemap.xml to include new sitemap location for better indexing

--- kavodfin/next-sitemap.config.js
+++ kavodfin/next-sitemap.config.js
@@ -3,4 +3,8 @@ module.exports = {
   siteUrl: 'https://kavodfin.com.ng',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+    await config.transform(config, '/details'),
+  ],
 };
