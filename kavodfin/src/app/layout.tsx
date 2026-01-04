eb9b9978521ca4728952c53f197c2343659f3d3c
Nel-tech <favournelson82@gmail.com>
2026-01-05 00:11:49 +0100

    FB-TEST

--- kavodfin/src/app/layout.tsx
+++ kavodfin/src/app/layout.tsx
@@ -52,8 +52,7 @@ export const metadata: Metadata = {
 
   
   other: {
-    "facebook-domain-verification":
-  process.env.NEXT_PUBLIC_FB_DOMAIN_VERIFICATION!,
+    "facebook-domain-verification":"TEST_123_ABC",
 
   },
 };
